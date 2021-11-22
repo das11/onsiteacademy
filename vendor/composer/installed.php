3fc59748918f704da442823c03da90a38186fff3
root <root@ip-172-31-10-96.us-east-2.compute.internal>
2021-11-22 12:18:07 +0000

    production

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '49d7ecdef3f50949e14cdff6405c442633bb0aa3',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -410,12 +410,12 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '49d7ecdef3f50949e14cdff6405c442633bb0aa3',
             'dev_requirement' => false,
         ),
         'laravel/socialite' => array(
